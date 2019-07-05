73bbaa25a54224d0292aec6d781c6d31de95d9d1
Manjunatha D <manjunatha.d@rubrik.com>
2019-07-06 00:34:52 +0530

    Setup GraphQL server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-const test: number = 20;
-console.log(test);
+const {GraphQLServer} = require('graphql-yoga');
+
+const typeDefs = `
+  type Query {
+    info: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    info: () => 'This is the API for Hackernews clone',
+  },
+};
+
+const graphqlServer = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+graphqlServer.start(() => console.log('Server running at port 4000'));
